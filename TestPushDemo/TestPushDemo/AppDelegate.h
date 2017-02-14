150de2361280afa6aab84b5bbefa21fa8e41adf9
Chaiyahang <yxczwlcyh@gmail.com>
2017-02-14 15:28:29 +0800

    创建项目

---
+++ TestPushDemo/TestPushDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestPushDemo
+//
+//  Created by CYH on 2017/2/14.
+//  Copyright © 2017年 Lianxi.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
